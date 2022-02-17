e80fed2b2ae453c065170de61b2d3e291cd3c7ea
DiegoBastos40 <diegobastosherval@gmail.com>
2022-02-17 08:52:51 +0000

    without style

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
+
 // default value for title local
 const projectName = 'lab-express-basic-auth';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
@@ -27,6 +28,9 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const authRouter = require('./routes/auth.routes'); // <== has to be added
+app.use('/', authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- models/User.model.js
+++ models/User.model.js
@@ -4,7 +4,13 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
+    trim: true,
+    required: [true, 'Username is required.'],
     unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: [true, 'Password is required.']
   },
   password: String
 });
